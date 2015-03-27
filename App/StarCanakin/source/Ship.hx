7d1c25858b65369303eb563ba48c5be77da30d0d
Bohdan Taraba <tbohdan@seznam.cz>
2015-03-27 22:22:09 +0100

    rules to init stats

--- App/StarCanakin/source/Ship.hx
+++ App/StarCanakin/source/Ship.hx
@@ -18,7 +18,8 @@ class Ship extends FlxSpriteGroup
 	var weaponPower:Int;
 	var energyLevel:Int;
 	var luck:Int;
-	var level:Int;
+	
+	public var level:Int;
 
 	public function new() 
 	{
@@ -27,12 +28,21 @@ class Ship extends FlxSpriteGroup
 	
 	private function initStats(level:Int = 1) 
 	{
+		// celkove mame k dispozici 6 statsu
+		// maximalni level hrace bude 10
+		// za kazdy novy level hrac dostane 1 ktery muze investovat
+		// celkovy startovaci pocet statsu je 3 * 6 + level. 
+		// tzn : 1 level - 18 statsu.
+		// 5 level - 23 statsu
+		// minimalni startovaci pocet bodu v kazdem statsu je 1, maximalni je 4 + level / 2. (?) 
+		// algoritmus na tvoreni statsu musi pri tvoreni lode vzdycky vycerpat vsechny startovaci statsy
+		
 		hitpoints = 10;
 		shield = 10;
 		shieldRecovery = 10;
 		weaponPower = 10;
 		energyLevel = 10;
-		luck = 10;	
+		luck = 10;
 	}
 	
 }
