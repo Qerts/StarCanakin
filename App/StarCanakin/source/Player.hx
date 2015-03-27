e252752fc41072a5678398e9b72d526ce12661a2
Bohdan Taraba <tbohdan@seznam.cz>
2015-03-27 13:14:15 +0100

    Revert "Revert "added sprites""
    This reverts commit 20c85331b83a715b0dea27e3908be75fd35dbdc7.

---
+++ App/StarCanakin/source/Player.hx
@@ -0,0 +1,39 @@
+package;
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup;
+
+class Player extends FlxSpriteGroup
+{
+	private static var player:Player;
+	
+	var playerSprite:FlxSprite;
+	
+	public function new() 
+	{
+		super();
+		playerSprite = new FlxSprite();
+		add(playerSprite);
+	}
+	
+	public static inline function getPlayer():Player
+	{
+		if (player == null)
+			return player = new Player();
+		else
+			return player;
+	}
+	
+	
+	override function update():Void
+	{
+
+	}
+	
+	
+	
+	private function evade() { }
+	private function attack() { }
+	private function increaseOffense() { }
+	private function increaseDefense() { }
+	
+}
