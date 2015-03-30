890af32ef1154764c4f70810cd81dc8febd6c46a
Bohdan Taraba <tbohdan@seznam.cz>
2015-03-30 23:53:20 +0200

    implementace prvni verze generatoru lodi
    + testovaci assety

--- App/StarCanakin/source/ShipGenerator.hx
+++ App/StarCanakin/source/ShipGenerator.hx
@@ -1,6 +1,7 @@
 package ;
 import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup;
+import flixel.util.FlxRandom;
 
 /**
  * ...
@@ -12,7 +13,34 @@ class ShipGenerator
 	public static function getShip():FlxSpriteGroup
 	{
 		var ship = new FlxSpriteGroup();
-		ship.add(new FlxSprite("assets/images/starship_pattern.png"));
+		
+		var bodyPart = FlxRandom.intRanged(0, 2);
+		var wingPart = FlxRandom.intRanged(0,2);
+		
+		
+		switch(wingPart)
+		{
+			case 0:
+				ship.add(new FlxSprite("assets/images/wings1.png"));
+			case 1:
+				ship.add(new FlxSprite("assets/images/wings2.png"));
+			case 2:
+				ship.add(new FlxSprite("assets/images/wings3.png"));
+		}
+		
+		switch(bodyPart)
+		{
+			case 0:
+				ship.add(new FlxSprite("assets/images/body1.png"));
+			case 1:
+				ship.add(new FlxSprite("assets/images/body2.png"));
+			case 2:
+				ship.add(new FlxSprite("assets/images/body3.png"));
+		}
+		
+		
+		
+		//ship.add(new FlxSprite("assets/images/starship_pattern.png"));
 		return ship;
 	}
 	
